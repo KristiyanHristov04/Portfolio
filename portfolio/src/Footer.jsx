982a2c964b6f1a932de71c8a92b2e30373cdb1b4
Kristiyan <kristiyan_hristov04@abv.bg>
2024-01-05 21:50:24 +0200

    Removed Unnecessary Files From The Project

--- portfolio/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import './Footer.css'
-
-export default function Footer() {
-    return (
-        <div className="footer">
-            <p>&#169; Copyright - Kristiyan Krasimirov Hristov | All Rights Reserved</p>
-        </div>
-    )
-}
